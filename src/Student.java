27e03ecafa25f95d166af797f158a601e02eed03
Batool AbuRomman <batool.aburomman@gmail.com>
2020-04-22 20:26:03 +0300

    Add Getter & Setter

--- src/Student.java
+++ src/Student.java
@@ -3,4 +3,11 @@ public class Student {
 private  int id;
 private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
